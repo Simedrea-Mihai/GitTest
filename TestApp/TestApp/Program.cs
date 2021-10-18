78b2ba4ff3e11748e107d15b2b365f1fcca929ad
Mihai Simedrea <mihai.simedrea@amdaris.com>
2021-10-18 15:52:32 +0300

    Dev1

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -8,9 +8,9 @@ namespace TestApp
         {
             Console.WriteLine("Message test 1");
             int i = 0;
-            while (i < 10)
+            while (i < 5) // am scazut numarul de iteratii
             {
-                Console.WriteLine("Test");
+                Console.WriteLine("Test {0}", i); // am modificat linia 13
 
                 i++; 
             }
